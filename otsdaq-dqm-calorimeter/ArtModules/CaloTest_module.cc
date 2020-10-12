156428f385b1e357b8ab585f249431a75c92856a
Greg <grakness@FNAL.GOV>
2020-10-12 03:05:06 -0500

    First version of the calo DQM repository

---
+++ otsdaq-dqm-calorimeter/ArtModules/CaloTest_module.cc
@@ -0,0 +1,193 @@
+//Author: S Middleton
+//Date: 2020
+//Purpose: prototype Analyzer fir DQM trigger rates. Based on Offline module Trigger/srs/ots::ProtoType. Much of the code was copied over from that module but has been adapted for Online vicualization.
+
+//Art:
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Core/ModuleMacros.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art/Framework/Principal/Run.h"
+#include "canvas/Utilities/InputTag.h"
+#include "art_root_io/TFileService.h"
+#include "art_root_io/TFileDirectory.h"
+#include "canvas/Persistency/Common/TriggerResults.h"
+#include "art/Framework/Services/System/TriggerNamesService.h"
+#include "cetlib_except/exception.h"
+#include "fhiclcpp/ParameterSet.h"
+#include "fhiclcpp/ParameterSetRegistry.h"
+
+#include "artdaq-core/Data/ContainerFragment.hh"
+#include "artdaq-core/Data/Fragment.hh"
+#include "artdaq/DAQdata/Globals.hh"
+
+#include "cetlib_except/exception.h"
+
+//OTS:
+#include "otsdaq/Macros/CoutMacros.h"
+#include "otsdaq/MessageFacility/MessageFacility.h"
+
+//ROOT:
+//#include "art/Framework/Services/Optional/TFileService.h"
+#include "art_root_io/TFileService.h" 
+#include <TBufferFile.h>
+#include <TH1F.h>
+#include <TH2F.h>
+#include <TH1.h>
+#include <TProfile.h>
+
+//Offline:
+#include <ConditionsService/inc/AcceleratorParams.hh>
+#include <ConditionsService/inc/ConditionsHandle.hh>
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wtype-limits" 
+#pragma GCC diagnostic ignored "-Wpedantic"
+#include <BFieldGeom/inc/BFieldManager.hh>
+#include <TrackerGeom/inc/Tracker.hh>
+//#include <GlobalConstantsService/inc/GlobalConstantsHandle.hh">
+#pragma GCC diagnostic pop
+
+#include <GeometryService/inc/GeomHandle.hh>
+#include <GeometryService/inc/DetectorSystem.hh>
+#include <RecoDataProducts/inc/CaloCluster.hh>
+#include <RecoDataProducts/inc/CaloTrigSeed.hh>
+#include <RecoDataProducts/inc/HelixSeed.hh>
+#include <RecoDataProducts/inc/KalSeed.hh>
+#include <RecoDataProducts/inc/TriggerInfo.hh>
+#include <RecoDataProducts/inc/ComboHit.hh>
+#include <RecoDataProducts/inc/StrawDigi.hh>
+#include <RecoDataProducts/inc/StrawDigiCollection.hh>
+#include <RecoDataProducts/inc/CaloDigi.hh>
+#include <RecoDataProducts/inc/CaloDigiCollection.hh>
+#include <DataProducts/inc/XYZVec.hh>
+
+#include <MCDataProducts/inc/SimParticle.hh>
+#include <MCDataProducts/inc/SimParticleCollection.hh>
+#include <MCDataProducts/inc/StrawDigiMC.hh>
+#include <MCDataProducts/inc/StrawDigiMCCollection.hh>
+#include <MCDataProducts/inc/StepPointMC.hh>
+#include <MCDataProducts/inc/StepPointMCCollection.hh>
+#include <MCDataProducts/inc/ProtonBunchIntensity.hh>
+
+
+#include <GlobalConstantsService/inc/ParticleDataTable.hh>
+
+//Utilities
+#include <Mu2eUtilities/inc/TriggerResultsNavigator.hh>
+#include <Mu2eUtilities/inc/HelixTool.hh>
+
+//OTS:
+#include "otsdaq-dqm-calorimeter/ArtModules/CaloTestHistos.h"
+#include "otsdaq/MessageFacility/MessageFacility.h"
+#include "otsdaq/Macros/CoutMacros.h"
+#include "otsdaq/Macros/ProcessorPluginMacros.h"
+#include "otsdaq/NetworkUtilities/TCPPublishServer.h"
+//C++:
+#include <algorithm>
+#include <functional>
+#include <initializer_list>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <sstream>
+#include <unordered_map>
+#include <vector>
+
+namespace ots
+{
+    class CaloTest : public art::EDAnalyzer
+    {
+      public:
+	    explicit CaloTest(fhicl::ParameterSet const& pset);
+	    virtual ~CaloTest();
+
+	    void analyze(art::Event const& e) override;
+	    void beginRun(art::Run const&) override;
+        void beginJob() override;
+        void endJob() override;
+	   
+        void PlotRate(art::Event const& e);
+    private:
+        art::RunNumber_t current_run_;
+        std::string outputFileName_;
+        art::ServiceHandle<art::TFileService> tfs;
+        bool writeOutput_;
+        bool doStreaming_;
+        bool overwrite_mode_;
+
+        art::InputTag _trigAlgTag;
+        art::InputTag _sdMCTag;
+        art::InputTag _sdTag;
+        
+        double _duty_cycle;
+        string _processName;
+
+        float _nProcess;
+        double _bz0;
+
+        double _nPOT;
+          
+        const mu2e::Tracker*      _tracker;
+        const mu2e::StrawDigiMCCollection* _mcdigis;
+       
+        const art::Event*                  _event;
+        CaloTestHistos *histos = new CaloTestHistos("hCalo");
+        TCPPublishServer *tcp ;
+        
+    };
+}
+
+ots::CaloTest::CaloTest(fhicl::ParameterSet const& pset)
+    : art::EDAnalyzer(pset),
+    current_run_(0),
+    outputFileName_(pset.get<std::string>("fileName", "DQMCaloTest.root")),
+    writeOutput_(pset.get<bool>("write_to_file", true)),
+    doStreaming_(pset.get<bool>("stream_to_screen", true)),
+    overwrite_mode_(pset.get<bool>("overwrite_output_file", true)),
+    _duty_cycle    (pset.get<float> ("dutyCycle", 1.)),
+    _processName   (pset.get<string> ("processName", "caloTest")),
+    _nProcess      (pset.get<float> ("nEventsProcessed", 1.)),
+    tcp(new TCPPublishServer(pset.get<int>("listenPort", 6000)))
+  {
+    TLOG_INFO("CaloTest") << "CaloTest construction is beginning " << TLOG_ENDL; 
+    TLOG_DEBUG("CaloTest") << "CaloTest construction complete" << TLOG_ENDL;
+ }
+
+ots::CaloTest::~CaloTest() {}
+
+
+
+void ots::CaloTest::beginJob(){
+  TLOG_INFO("CaloTest - BeginJob")
+	    << "Started" << TLOG_ENDL;
+    histos->BookHistos(tfs);
+}
+
+void ots::CaloTest::analyze(art::Event const& event)
+{
+	TLOG_INFO("CaloTest - Event")
+	    << "CaloTest Module is Analyzing Event #  " << event.event() << TLOG_ENDL;
+    double value = 100.;
+    histos->CaloTest._FirstCaloHist->Fill(value);
+    TBufferFile message(TBuffer::kWrite);
+	message.WriteObject(histos->CaloTest._FirstCaloHist);
+
+   //__CFG_COUT__ << "Broadcasting!" << std::endl;
+   tcp->broadcastPacket(message.Buffer(), message.Length());
+
+}
+
+
+  void ots::CaloTest::endJob(){
+    TLOG_INFO("CaloTest - EndJob")
+	    << "Completed" << TLOG_ENDL;
+  }
+
+ void ots::CaloTest::beginRun(const art::Run & run){
+ 
+  }
+
+
+
+
+DEFINE_ART_MODULE(ots::CaloTest)
---
+++ otsdaq-dqm-calorimeter/DataProcessorPlugins/DQMCaloHistoConsumer.h
@@ -0,0 +1,48 @@
+#ifndef _ots_DQMCaloHistoConsumer_h_
+#define _ots_DQMCaloHistoConsumer_h_
+
+#include "otsdaq/DataManager/DQMHistosConsumerBase.h"
+#include "otsdaq/Configurable/Configurable.h"
+#include "otsdaq-dqm/ArtModules/ProtoTypeHistos.h"
+#include <string>
+
+class TFile;
+class TDirectory;
+class TH1F;
+class TTree;
+
+namespace ots
+{
+
+	class DQMCaloHistoConsumer : public DQMHistosConsumerBase, public Configurable
+	{
+		public:
+	  		DQMCaloHistoConsumer(std::string supervisorApplicationUID, std::string bufferUID, std::string processorUID, const ConfigurationTree& theXDAQContextConfigTree, const std::string& configurationPath);
+			virtual ~DQMCaloHistoConsumer(void);
+
+			void startProcessingData (std::string runNumber) override;
+			void stopProcessingData  (void) override;
+			void pauseProcessingData (void) override;
+			void resumeProcessingData(void) override;
+			void load                (std::string fileName){;}
+
+		private:
+			bool workLoopThread(toolbox::task::WorkLoop* workLoop);
+			void fastRead(void);
+			void slowRead(void);
+	
+			//For fast read
+			std::string*                       dataP_;
+			std::map<std::string,std::string>* headerP_;
+
+			bool                               saveFile_; //yes or no
+			std::string                        filePath_;
+			std::string                        radixFileName_;
+
+			TH1F*                              hGauss_;//TODO need to add our own class here
+			TTree*                             testTree_;
+			ProtoTypeHistos                    testHistos_;
+	};
+}
+
+#endif
